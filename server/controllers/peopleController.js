ee0907a1350d0e544d4a34b23acf07268a68d7ac
BenDehlin <ben.dehlin@gmail.com>
2020-05-28 01:59:44 -0600

    minor refactor controller wording

--- server/controllers/peopleController.js
+++ server/controllers/peopleController.js
@@ -1,38 +1,37 @@
 module.exports = {
-    getPerson: (req, res) => {
-        const db = req.app.get('db')
-        const {id} = req.params
-        db.get_person(id).then(results => {
-            const person = results[0]
-            res.status(200).send(person)
-        }).catch(err => res.status(500).send(err))
-    },
-    getPeople: (req, res) => {
-        const db = req.app.get('db')
-        db.get_people().then(results => {
-            res.status(200).send(results)
-        }).catch(err => res.status(500).send(results))
-    },
-    addPerson: (req, res) => {
-        const db = req.app.get('db')
-        const {name, age} = req.body
-        db.add_person(name, age).then(results => {
-            res.status(200).send(results)
-        }).catch(err => res.status(500).send(err))
-    },
-    deletePerson: (req, res) => {
-        const db = req.app.get('db')
-        const {id} = req.params
-        db.delete_person(id).then(results => {
-            res.status(200).send(results)
-        }).catch(err => res.status(500).send(err))
-    },
-    editPerson: (req, res) => {
-        const db = req.app.get('db')
-        const {id} = req.params
-        const {name, age} = req.body
-        db.edit_person(id, name, age).then(results => {
-            res.status(200).send(results)
-        }).catch(err => res.status(500).send(err))
-    }
+  getPerson: (req, res) => {
+    const db = req.app.get("db")
+    const { id } = req.params
+    db.get_person(id)
+      .then((results) => res.status(200).send(results[0]))
+      .catch((err) => res.status(500).send(err))
+  },
+  getPeople: (req, res) => {
+    const db = req.app.get("db")
+    db.get_people()
+      .then((results) => res.status(200).send(results))
+      .catch((err) => res.status(500).send(err))
+  },
+  addPerson: (req, res) => {
+    const db = req.app.get("db")
+    const { name, age } = req.body
+    db.add_person(name, age)
+      .then(() => res.sendStatus(200))
+      .catch((err) => res.status(500).send(err))
+  },
+  deletePerson: (req, res) => {
+    const db = req.app.get("db")
+    const { id } = req.params
+    db.delete_person(id)
+      .then(() => res.sendStatus(200))
+      .catch((err) => res.status(500).send(err))
+  },
+  editPerson: (req, res) => {
+    const db = req.app.get("db")
+    const { id } = req.params
+    const { name, age } = req.body
+    db.edit_person(id, name, age)
+      .then(() => res.sendStatus(200))
+      .catch((err) => res.status(500).send(err))
+  },
 }
